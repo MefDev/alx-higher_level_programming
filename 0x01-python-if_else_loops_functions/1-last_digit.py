31c074d08719ec0ddbe5de561ef869b060e9fe2b
mefdev <mohamedmeftah2018@gmail.com>
2023-08-08 23:20:21 +0100

    Last digit in check

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,19 +2,20 @@
 import random
 number = random.randint(-10000, 10000)
 
+
 def last_digit(num):
     last_digit_unsigned = abs(num) % 10
     return -last_digit_unsigned if (num < 0) else last_digit_unsigned
+
 
 last_digit_val = last_digit(number)
 stringVal = "Last digit of"
 greatThanFive = "and is greater than 5"
 lessThanSix = "and is less than 6 and not 0"
 isZero = "and is 0"
-
 if (last_digit_val > 5):
     print(f"{stringVal} {number} is {last_digit_val} {greatThanFive}")
 elif last_digit_val == 0:
-     print(f"{stringVal} {number} is {last_digit_val} {isZero}")
+    print(f"{stringVal} {number} is {last_digit_val} {isZero}")
 else:
     print(f"{stringVal} {number} is {last_digit_val} {lessThanSix}")

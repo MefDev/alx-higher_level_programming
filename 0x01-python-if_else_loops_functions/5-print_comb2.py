c46ea8012f4d62dfc22dcde597dda01b76a5e45a
mefdev <mohamedmeftah2018@gmail.com>
2023-08-09 00:36:46 +0100

    Print 2 digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if (i <= 9):
-        print("0", end="")
-    print("{:d}".format(i, i), end=", ")
+for i in range(0, 100):
+    print("{0:02d}".format(i), end=", ")
 print(99)

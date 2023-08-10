4e9e252ab8a2727bc7140ba21af26dd5bd5218ec
mefdev <mohamedmeftah2018@gmail.com>
2023-08-10 16:34:00 +0100

    Arguments like crazy

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+num_of_args = len(sys.argv) - 1
+if num_of_args == 0:
+    print(f"{num_of_args} arguments.")
+else:
+    print(f"{num_of_args} arguments:")
+    for i in range(1, num_of_args + 1):
+        print(f"{i}: {sys.argv[i]}") #

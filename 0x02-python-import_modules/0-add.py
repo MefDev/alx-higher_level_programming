e4fd931f8c29e421d8e9b145522a5d689fe46e9d
mefdev <mohamedmeftah2018@gmail.com>
2023-08-11 19:46:23 +0100

    ADD two nums

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,6 @@
 if __name__ == "__main__":
     """Print the sum of 1 and 2"""
     from add_0 import add
-    a, b = 1, 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

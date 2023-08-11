266b27e788b03e66753f0ea93d29ca06ac5c1ef9
mefdev <mohamedmeftah2018@gmail.com>
2023-08-11 20:10:00 +0100

    TEST Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-num_of_args = len(sys.argv) - 1
-if num_of_args == 0:
-    print("0 arguments.")
-else:
-    if num_of_args == 1:
-        print("1 argument:")
-        print("{}: {}".format(1, sys.argv[1]))
+if __name__ == "__main__":
+    num_of_args = len(sys.argv) - 1
+    if num_of_args == 0:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, num_of_args + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+        if num_of_args == 1:
+            print("1 argument:")
+            print("{}: {}".format(1, sys.argv[1]))
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+            for i in range(1, num_of_args + 1):
+                print("{}: {}".format(i, sys.argv[i]))

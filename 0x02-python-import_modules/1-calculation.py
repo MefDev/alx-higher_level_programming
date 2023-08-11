06103997ec1c7f43da4c37ed9f8ea61f715e6f21
mefdev <mohamedmeftah2018@gmail.com>
2023-08-11 19:51:29 +0100

    Perform calculation based on the func

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,55 +1,10 @@
 #!/usr/bin/python3
-def add(a=10, b=5):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    print(f"{a} + {b} = {a + b}")
-    return (a + b)
-
-
-def sub(a=10, b=5):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    print(f"{a} - {b} = {a - b}")
-    return (a - b)
-
-
-def mul(a=10, b=5):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    print(f"{a} * {b} = {a * b}")
-    return (a * b)
-
-
-def div(a=10, b=5):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    print(f"{a} / {b} = {int(a / b)}")
-    return int(a / b)
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5"""
+    from calculator_1 import div, add, sub, mul
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))

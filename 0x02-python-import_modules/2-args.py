3ebf4911d73a8c62c40566ff1a0da9a1e85e1269
mefdev <mohamedmeftah2018@gmail.com>
2023-08-11 20:00:45 +0100

    TEST THAT THING

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,11 @@
 import sys
 num_of_args = len(sys.argv) - 1
 if num_of_args == 0:
-    print(f"{num_of_args} arguments.")
+    print("0 arguments.")
 else:
-    print(f"{num_of_args} arguments:")
-    for i in range(1, num_of_args + 1):
-        print(f"{i}: {sys.argv[i]}")
+    if num_of_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, num_of_args + 1):
+            print("{} {} arguments:".format(i, sys.argv[i]))

4d046837a71229fd7f2e150d47610dc8ea029687
mefdev <mohamedmeftah2018@gmail.com>
2023-08-11 20:01:36 +0100

    TEST THAT THING

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ else:
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, num_of_args + 1):
-            print("{} {} arguments:".format(i, sys.argv[i]))
+            print("{}: {} arguments:".format(i, sys.argv[i]))

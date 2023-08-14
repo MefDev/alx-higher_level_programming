fa8e3f0d2b000fbddbbdab1e5354a4865f75c718
mefdev <mohamedmeftah2018@gmail.com>
2023-08-14 20:04:46 +0100

    hox fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for number in list:
-            if (number == list[-1]):
+            if (number % 3 != 0):
                 print("{:d}".format(number), end=" ")
             else:
                 print("{:d}".format(number), end="")

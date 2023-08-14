cd349e86bd7e712cc2a9953fc9ba6103bcbec65e
mefdev <mohamedmeftah2018@gmail.com>
2023-08-14 20:52:39 +0100

    Hot fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,3 +17,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 return (tuple_a[0] + tuple_b[0]), (0 + tuple_b[1])
             else:
                 return (0 + tuple_b[0]), (tuple_a[1] + tuple_b[1])
+        else:
+            return ((0 + tuple_b[0]), (0 + tuple_b[1]))

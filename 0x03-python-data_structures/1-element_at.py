54f58f6c310fbe8daa471f523f73897401f244f4
mefdev <mohamedmeftah2018@gmail.com>
2023-08-14 17:38:48 +0100

    Retrieve the the number depending on the idx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 if __name__=="__name__":
     def element_at(my_list, idx):
         if idx < 0 or idx > len(my_list):
-            return None
+            return "None"
         else:
             return my_list[idx]

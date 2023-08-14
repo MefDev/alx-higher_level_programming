541c68891924e430a2778acdc25a82328a3be26e
mefdev <mohamedmeftah2018@gmail.com>
2023-08-14 19:22:34 +0100

    No c or C in my text

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char != "c" and char != "C":
+            char = '0'
+        else:
             new_string += char
     return new_string

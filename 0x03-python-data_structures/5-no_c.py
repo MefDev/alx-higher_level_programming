2037380670aa44e6d7423e98059220b2dce8dee2
mefdev <mohamedmeftah2018@gmail.com>
2023-08-14 18:23:44 +0100

    No c or C in my text

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     new_string = []
     for char in my_string:
         if char == "c" or char == "C":
-            i = '0'
+            char = '0'
         else:
             new_string.append(char)
     return "".join(new_string)

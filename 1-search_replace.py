59480fd34ec61f7bdd2c02bced6c2e453f5cf129
mefdev <mohamedmeftah2018@gmail.com>
2023-08-16 22:15:29 +0100

    search & replace item in a list

--- 1-search_replace.py
+++ 1-search_replace.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    return ("".join(str(new_list).replace(str(search), str(replace))))
+    for i in new_list:
+        index = new_list.index(search)
+        if index in new_list:
+            new_list[index] = replace
+    return new_list

355f53f9824fee6541c91c8c322ab909d959e397
mefdev <mohamedmeftah2018@gmail.com>
2023-08-16 23:34:24 +0100

    Update the dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     new_dictionary = a_dictionary.copy()
     if key in new_dictionary:
         a_dictionary[key] = value
-    else:
+    if key not in new_dictionary:
         a_dictionary[key] = value
     return new_dictionary

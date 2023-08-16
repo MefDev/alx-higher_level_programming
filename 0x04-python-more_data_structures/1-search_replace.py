9ca5945dc860128c4b9e753e43019dc2c4dc251b
mefdev <mohamedmeftah2018@gmail.com>
2023-08-16 22:21:33 +0100

    search & replace item in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for i in new_list:
-        index = new_list.index(search)
-        if index in new_list:
-            new_list[index] = replace
-        else:
-            return new_list
-    return new_list
+    return ("".join(str(new_list).replace(str(search), str(replace))))

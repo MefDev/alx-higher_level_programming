d7942bc6e411f2f85c7f1ef3e978dd3b360c896c
mefdev <mohamedmeftah2018@gmail.com>
2023-08-17 01:30:32 +0100

    hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum = 0
+    if not roman_string:
+        return 0
     for char in roman_string:
         for key, val in dic.items():
             if (char == key):

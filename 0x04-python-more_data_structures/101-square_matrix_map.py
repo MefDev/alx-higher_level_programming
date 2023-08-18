d1b2792d266d270ed65e1f5d70dbcd60ea750250
mefdev <mohamedmeftah2018@gmail.com>
2023-08-18 16:28:35 +0100

    Square each num in the matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    return list(map(lambda lst: list(map(lambda x: x**2, lst)), new_matrix))
+    return list(map(lambda lst: list(map(lambda x: x**2, lst)), matrix.copy()))

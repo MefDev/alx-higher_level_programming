464c884b1b279652ad1bcc09d10df7a237dfdbf9
mefdev <mohamedmeftah2018@gmail.com>
2023-08-29 00:04:50 +0100

    Safe print with integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,3 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
         return count
     except IndexError as IndexErr:
         print(IndexErr)
+    except TypeError as TypeErr:
+        print(TypeErr)

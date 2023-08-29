15903aae0c47913cb8116ded1f13929e80b125a4
mefdev <mohamedmeftah2018@gmail.com>
2023-08-29 20:01:33 +0100

    TEST it out

--- 100-safe_print_integer_err.py
+++ 100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end="\n")
         return True
-    except TypeError as typeErr:
-        sys.stderr.write("Exception: {}".format(typeErr))
     except ValueError as valErr:
         sys.stderr.write("Exception: {}".format(valErr))
+    except TypeError as typeErr:
+        sys.stderr.write("Exception: {}".format(typeErr))

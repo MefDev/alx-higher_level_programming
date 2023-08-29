36bac55e13af1d0589647bcc1fe70118536faf8a
mefdev <mohamedmeftah2018@gmail.com>
2023-08-29 21:02:50 +0100

    Safe print the function output

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
     import sys
-    if (len(args) < 2):
-        print("Exception: Not enough arguments provided")
-    else:
-        a = args[0]
-        b = args[1]
     try:
-        div = fct(a, b)
+        div = fct(*args)
         return div
     except (ZeroDivisionError, IndexError) as err:
         sys.stderr.write("Exception: {}\n".format(err))

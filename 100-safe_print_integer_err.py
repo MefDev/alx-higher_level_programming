eb787c45701c3732af4542f01c95406b57d36ec6
mefdev <mohamedmeftah2018@gmail.com>
2023-08-29 20:04:56 +0100

    Safe print integer with magicians effects

--- 100-safe_print_integer_err.py
+++ 100-safe_print_integer_err.py
@@ -6,7 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end="\n")
         return True
-    except ValueError as valErr:
-        sys.stderr.write("Exception: {}".format(valErr))
-    except TypeError as typeErr:
-        sys.stderr.write("Exception: {}".format(typeErr))
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+   

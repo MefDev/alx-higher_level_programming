a3b36fe60030f58230a01c9a184869910e81d6b9
mefdev <mohamedmeftah2018@gmail.com>
2023-08-30 01:56:43 +0100

    Square using setters and getters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,20 +9,23 @@ class Square:
     """ create basic class """
 
     def __init__(self, size=0):
-        self.set_size(size)
+        self.size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
 
+    @property
     def get_size(self):
-        return self.size
+        return self.__size
 
-    def set_size(self, value):
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
         self.size = value
 
     def area(self):
-        if not isinstance(self.size, int):
-            raise TypeError("size must be an integer")
-        else:
-            return pow(self.size, 2)
+        return pow(self.size, 2)

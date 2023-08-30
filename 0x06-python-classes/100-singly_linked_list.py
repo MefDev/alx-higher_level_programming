e389045e36f2ed49ac31ad04ceb25a8c91ee0505
mefdev <mohamedmeftah2018@gmail.com>
2023-08-31 00:36:50 +0100

    implement singly linked list in python

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -26,19 +26,19 @@ class Node:
 
     @property
     def data(self):
-       
+
         return self.__data
 
     @data.setter
     def data(self, value):
-        
+
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-       
+
         return self.__next_node
 
     @next_node.setter

43eb32d9e2b56179b7eb0c98ebb3278bf5ec4eb2
mefdev <mohamedmeftah2018@gmail.com>
2023-08-30 02:03:36 +0100

    Square using setters and getters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,6 +14,7 @@ class Square:
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
+
     @property
     def get_size(self):
         return self.size

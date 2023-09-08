6f3eb8b1556e43f66c0c5835d82d9eaef603e73d
mefdev <mohamedmeftah2018@gmail.com>
2023-09-08 22:48:33 +0100

    Add the presentation of an obj

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -57,5 +57,6 @@ class Rectangle:
         if not self.perimeter():
             return ""
         return "\n".join(["#" * self.width for _ in range(self.height)])
+
     def __repr__(self):
         return "Rectangle({}, {})".format(self.width, self.height)

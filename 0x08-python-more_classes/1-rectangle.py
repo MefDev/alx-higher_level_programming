29a352aac549f7c1a60b9a9ad66e5b9424d5dd9d
mefdev <mohamedmeftah2018@gmail.com>
2023-09-08 21:44:10 +0100

    Define a class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,8 @@ class Rectangle:
         if (isinstance(value, int)):
             if (value < 0):
                 raise ValueError("width must be >= 0")
-            self.__width = value
+            else:
+                self.__width = value
         else:
             raise TypeError("width must be an integer")
 
@@ -31,6 +32,7 @@ class Rectangle:
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("height must be >= 0")
-            self.__height = value
+            else:
+                self.__height = value
         else:
             raise TypeError("height must be an integer")

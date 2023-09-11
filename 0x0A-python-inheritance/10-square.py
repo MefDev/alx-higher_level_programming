09c9f690ad4cfcd649bd0eabaa04e632d5a34978
mefdev <mohamedmeftah2018@gmail.com>
2023-09-11 21:32:53 +0100

    Sqaure it

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class square (based on 9-rectangle.py)"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """creates a sqaure class"""
+
+    def __init__(self, size):
+        """initializes the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)

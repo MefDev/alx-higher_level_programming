c724f4330b9cd69cab1cf23fb01a755ec140d920
mefdev <mohamedmeftah2018@gmail.com>
2023-09-11 19:24:51 +0100

    Add geo

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,9 +6,11 @@ class BaseGeometry:
     """class BaseGeometry"""
 
     def area(self):
+        """AREA BaseGeometry"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Integer validation BaseGeometry"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:

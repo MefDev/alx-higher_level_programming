f343d0c02179ca38df670bfe936fc689e4b3aec4
mefdev <mohamedmeftah2018@gmail.com>
2023-09-12 22:01:30 +0100

    Read from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
+    """Read from a file then print"""
     with open(filename, encoding="UTF-8") as a_file:
         print(a_file.read())

81f0f4f616ff17d54bac8191fee2c6e216b9432a
mefdev <mohamedmeftah2018@gmail.com>
2023-09-13 16:50:53 +0100

    Implement pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+a list of lists of integers representing the Pascal’s triangle of n
+"""
+
+
 def pascal_triangle(n):
-    if (n <= 0):
-        return 0
+    """Pascal tri"""
+    pascal_tri_list = []
+    prevN = []
+    for i in range(n):
+        atN = [1] + [sum(prevN[ele:ele + 2]) for ele in range(len(prevN) - 1)]
+        if i > 0:
+            atN = atN + [1]
+        pascal_tri_list.append(atN)
+        prevN = atN
+    return pascal_tri_list

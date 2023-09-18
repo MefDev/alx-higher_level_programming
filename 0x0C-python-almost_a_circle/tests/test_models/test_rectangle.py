9654392ea53f3cff1b016d0bbd4db94c0741ffcb
mefdev <mohamedmeftah2018@gmail.com>
2023-09-18 23:05:29 +0100

    Add test for rec class

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,7 @@
 import unittest
 from models.rectangle import Rectangle
 
+
 class TestBase(unittest.TestCase):
     def test_constructor(self):
         """Test the constructor of Rectangle"""
@@ -34,3 +35,47 @@ class TestBase(unittest.TestCase):
         self.assertEqual(r.width, 100)
         self.assertEqual(r.x, 30)
         self.assertEqual(r.y, 25)
+
+    def test_valid_attributes(self):
+        # Test valid attributes with no exceptions raised
+        rect = Rectangle(5, 10, 2, 3)
+        self.assertEqual(rect.width, 5)
+        self.assertEqual(rect.height, 10)
+        self.assertEqual(rect.x, 2)
+        self.assertEqual(rect.y, 3)
+
+    def test_invalid_width(self):
+        # Test invalid width (not an integer)
+        with self.assertRaises(TypeError):
+            Rectangle("invalid", 10, 2, 3)
+
+        # Test width <= 0
+        with self.assertRaises(ValueError):
+            Rectangle(0, 10, 2, 3)
+
+    def test_invalid_height(self):
+        # Test invalid height (not an integer)
+        with self.assertRaises(TypeError):
+            Rectangle(5, "invalid", 2, 3)
+
+        # Test height <= 0
+        with self.assertRaises(ValueError):
+            Rectangle(5, 0, 2, 3)
+
+    def test_invalid_x(self):
+        # Test invalid x (not an integer)
+        with self.assertRaises(TypeError):
+            Rectangle(5, 10, "invalid", 3)
+
+        # Test x < 0
+        with self.assertRaises(ValueError):
+            Rectangle(5, 10, -2, 3)
+
+    def test_invalid_y(self):
+        # Test invalid y (not an integer)
+        with self.assertRaises(TypeError):
+            Rectangle(5, 10, 2, "invalid")
+
+        # Test y < 0
+        with self.assertRaises(ValueError):
+            Rectangle(5, 10, 2, -3)

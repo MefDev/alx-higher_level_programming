3d9caa5604e174e1e7f37dd046223c7161a8ee54
mefdev <mohamedmeftah2018@gmail.com>
2023-09-19 02:23:02 +0100

    sq

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,14 +1,49 @@
 #!/usr/bin/python3
+"""square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square class"""
+    """represent square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a square object"""
+        """initializes"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """get size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """updates attributes of square"""
+        if args:
+            i = 0
+            keys = ['id', 'size', 'x', 'y']
+            for arg in args:
+                setattr(self, keys[i], arg)
+                i += 1
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ returns the dictionary"""
+        dictionary = {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
+        return dictionary
+
     def __str__(self):
-        """String representation of the square"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        """print"""
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.size))

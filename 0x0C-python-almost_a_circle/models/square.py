a9e1dfe7bd80b5e65b909b089ebb3e7cd636a25c
mefdev <mohamedmeftah2018@gmail.com>
2023-09-19 02:18:49 +0100

    sq

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,37 +9,6 @@ class Square(Rectangle):
         """Initialize a square object"""
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        """Getter for size attribute"""
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """Setter for size attribute"""
-        self.width = value
-        self.height = value
-
-    def update(self, *args, **kwargs):
-        """Update attributes of the square"""
-        if args:
-            attribute_names = ['id', 'size', 'x', 'y']
-            for i, arg in enumerate(args):
-                setattr(self, attribute_names[i], arg)
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """Return a dictionary representation of the square"""
-        dictionary = {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-        }
-        return dictionary
-
     def __str__(self):
         """String representation of the square"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"

9b6dacac1bfb108b9c86ed4c7ef4ff733e814118
mefdev <mohamedmeftah2018@gmail.com>
2023-09-19 01:57:36 +0100

    Test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,70 +1,110 @@
 #!/usr/bin/python3
+"""rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Class Rectangle inherits from Base"""
+    """represent a rectangle class"""
+
+    print_symbol = '#'
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize the Rectangle"""
+        """initializes"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+
+    # function that validate att and value
+    def validator(self, att, value):
+        """validation of all setter """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(att))
+        if (att == 'width' or att == 'height') and value <= 0:
+            raise ValueError("{} must be > 0".format(att))
+        if (att == 'x' or att == 'y') and value < 0:
+            raise ValueError("{} must be >= 0".format(att))
+
+    def update(self, *args, **kwargs):
+        """updates attributes of rectongle """
+        if args:
+            i = 0
+            keys = ['id', 'width', 'height', 'x', 'y']
+            for arg in args:
+                setattr(self, keys[i], arg)
+                i += 1
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ returns the dictionary"""
+        dictionary = {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
+        return dictionary
 
     @property
     def width(self):
-        """Getter for width attribute"""
+        """get width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for width attribute"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
+        """set width"""
+        self.validator("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """Getter for height attribute"""
+        """get height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for height attribute"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
+        """set height"""
+        self.validator("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """Getter for x attribute"""
+        """get x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter for x attribute"""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
+        """set x"""
+        self.validator("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """Getter for y attribute"""
+        """get y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter for y attribute"""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
+        """set y"""
+        self.validator("y", value)
         self.__y = value
+
+    def area(self):
+        """return the area of rectongle"""
+        return self.width * self.height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        print('\n' * self.y, end="")
+        pattern = [' ' * self.x + "{}".format(self.print_symbol) *
+                   self.width for _ in range(self.height)]
+        print('\n'.join(pattern))
+
+    def __str__(self):
+        """print"""
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.x, self.y, self.width, self.height))

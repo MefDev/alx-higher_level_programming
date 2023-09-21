b3996be58f845f7b516d5452b287b4e10e77d7c1
mefdev <mohamedmeftah2018@gmail.com>
2023-09-21 10:07:11 +0100

    More tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,10 +12,10 @@ class TestBase(unittest.TestCase):
         b3 = Base()
         b4 = Base()
 
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
-        self.assertEqual(b4.id, 4)
+        self.assertEqual(b1.id, 3)
+        self.assertEqual(b2.id, 4)
+        self.assertEqual(b3.id, 5)
+        self.assertEqual(b4.id, 6)
 
     def test_custom_value(self):
         """Test the custom value of Base"""
@@ -27,27 +27,29 @@ class TestBase(unittest.TestCase):
 
     def test_mixed_values(self):
         """Test both custom value and auto increment of Base"""
-        b1 = Base()
         b2 = Base(120)
-        b3 = Base()
         b4 = Base(8)
         b5 = Base(16)
+        b6 = Base()
+        b7 = Base()
 
-        self.assertEqual(b1.id, 6)
+       
         self.assertEqual(b2.id, 120)
-        self.assertEqual(b3.id, 7)
         self.assertEqual(b4.id, 8)
         self.assertEqual(b5.id, 16)
+        self.assertEqual(b6.id, 8)
+        self.assertEqual(b7.id, 9)
+
 
     def test_json_custom_values(self):
         r1 = Rectangle(10, 7, 2, 8)
         dictionary = r1.to_dictionary()
         json_dictionary = Base.to_json_string([dictionary])
         self.assertEqual(
-            dictionary, {'id': 5, 'width': 10, 'height': 7, 'x': 2, 'y': 8})
+            dictionary, {'id': 7, 'width': 10, 'height': 7, 'x': 2, 'y': 8})
         self.assertEqual(type(dictionary), dict)
         self.assertEqual(
-            json_dictionary, '[{"id": 5, "width": 10, "height": 7, "x": 2, "y": 8}]')
+            json_dictionary, '[{"id": 7, "width": 10, "height": 7, "x": 2, "y": 8}]')
         self.assertEqual(type(json_dictionary), str)
 
     def test_json_empty_none_values(self):
@@ -65,7 +67,7 @@ class TestBase(unittest.TestCase):
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
         self.assertEqual(Rectangle.save_to_file(
-            [r1, r2]), '[{"id": 8, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 9, "width": 2, "height": 4, "x": 0, "y": 0}]')
+            [r1, r2]), '[{"id": 10, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 11, "width": 2, "height": 4, "x": 0, "y": 0}]')
 
     def test_save_to_file_None_values(self):
         r1 = None
@@ -82,3 +84,11 @@ class TestBase(unittest.TestCase):
         self.assertEqual(type(list_input), list)
         self.assertEqual(type(json_list_input), str)
         self.assertEqual(type(list_output), list)
+
+    def test_create_custom_values(self):
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+
+        self.assertEqual(r1 is r2, False)
+        self.assertEqual(r1 == r2, False)

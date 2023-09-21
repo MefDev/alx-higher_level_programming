6b6b50a0ca03d54a6ad31b7094a09fd2cb8073af
mefdev <mohamedmeftah2018@gmail.com>
2023-09-21 09:34:51 +0100

    Save to file classmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,10 +29,11 @@ class Base:
         """Writes the JSON string representation of list_objs to a file"""
         if list_objs is None:
             list_objs = []
-        jsons = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        json_objs = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
 
-        with open(f"{cls.__name__}.json", "w", encoding="utf-8") as file:
-            file.write(jsons)
+        with open(f"{cls.__name__}.json", "w", encoding="utf-8") as f:
+            f.write(json_objs)
+        return json_objs
 
     @staticmethod
     def from_json_string(json_string):

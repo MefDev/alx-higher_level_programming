31c5decd872efd37153d1769aac6c5a9097911b7
MefDev <mohamedmeftah2018@gmail.com>
2023-10-17 16:40:50 -0700

    Insert values into a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- SHOW all rows of the first_table in your MySQL server
-INSERT INTO first_table (id, name) VALUES (89, `Best School`);
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
 

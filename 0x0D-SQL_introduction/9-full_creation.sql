5f930a36b266b9846f4e64dba6a1a6e808ffa37e
MefDev <mohamedmeftah2018@gmail.com>
2023-10-17 17:01:00 -0700

    Full creation of db

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- FULL creation  of the second_table in your MySQL server
+
+CREATE TABLE NOT EXISTS second_table (id INT, name VARCHAR(256);
+	INSERT INTO second_table (id, name, score)
+	VALUES
+	(1, "John", 10),
+	(2, "Alex", score, 3),
+	(3, "Bob", 14),
+	(4, "George", 8);

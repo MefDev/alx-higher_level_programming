c2dd1f41cc2911320fc788f0c078411ce9fbcd19
MefDev <mohamedmeftah2018@gmail.com>
2023-10-19 15:29:19 -0700

    hot fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- SELECT FROM DB and its table
-SELECT cs.id, cs.name, ss.name FROM cities cs JOIN states ss ON cs.id = ss.id ORDER BY cs.id ASC;
+SELECT cs.id, cs.name, ss.name FROM cities cs JOIN states ss ON cs.state_id = ss.id ORDER BY cs.id ASC;
 

b6e45a4251bec5c64bea765c2df181df37685316
MefDev <mohamedmeftah2018@gmail.com>
2023-10-19 15:34:54 -0700

    Add that

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,5 @@
 -- SELECT FROM DB and its table
-SELECT tg.name AS genre, COUNT(tg.id) as number_of_shows FROM tv_genres tg JOIN tv_show_genres tsg ON tg.id = tsg.genre_id GROUP BY tsg.genre_id ORDER BY number_of_shows DESC;
-
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;

e6abfaa6c253b0ad56473e8fd37c26fb2dd2f61d
mefdev <mohamedmeftah2018@gmail.com>
2023-11-13 22:57:27 +0100

    Concat string

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,11 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 
 const listOfArgs = process.argv.slice(2);
 
-if (listOfArgs.length === 0){
-    console.log(listOfArgs[0] + " is " + listOfArgs[1])
-} else if (listOfArgs.length < 2){
-    console.log(listOfArgs[0] + " is " + listOfArgs[1])
-} else{
-    console.log(`${listOfArgs[0]} is ${listOfArgs[1]}`)
+if (listOfArgs.length === 0) {
+  console.log(listOfArgs[0] + ' is ' + listOfArgs[1]);
+} else if (listOfArgs.length < 2) {
+  console.log(listOfArgs[0] + ' is ' + listOfArgs[1]);
+} else {
+  console.log(`${listOfArgs[0]} is ${listOfArgs[1]}`);
 }

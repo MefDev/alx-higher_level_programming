5d01cd840a62651d29a7092f241116299ff4d648
mefdev <mohamedmeftah2018@gmail.com>
2023-11-13 22:56:24 +0100

    Concatination made easy

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,11 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
 const listOfArgs = process.argv.slice(2);
 
 if (listOfArgs.length === 0){
-    console.log(undefined + " is " + undefined)
+    console.log(listOfArgs[0] + " is " + listOfArgs[1])
 } else if (listOfArgs.length < 2){
-    console.log(listOfArgs[0]," is ", undefined)
+    console.log(listOfArgs[0] + " is " + listOfArgs[1])
 } else{
     console.log(`${listOfArgs[0]} is ${listOfArgs[1]}`)
 }

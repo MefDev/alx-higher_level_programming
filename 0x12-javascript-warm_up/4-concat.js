a6eafd24171b58e31df037d62885c1e96b6064bd
mefdev <mohamedmeftah2018@gmail.com>
2023-11-13 22:51:41 +0100

    Concatination made easy

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const listOfArgs = process.argv.slice(2);
+
+if (listOfArgs.length === 0) {
+  console.log('undefined' + ' is ' + 'undefined');
+} else if (listOfArgs.length < 2) {
+  console.log(listOfArgs[0], ' is ', 'undefined');
+} else {
+  console.log(`${listOfArgs[0]} is ${listOfArgs[1]}`);
+}

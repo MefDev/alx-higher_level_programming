cb237c70f1ad0bae4d2c87d9a29e719ff84eab7d
mefdev <mohamedmeftah2018@gmail.com>
2023-11-14 00:37:31 +0100

    Second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let listOfArgs = process.argv.slice(2);
+listOfArgs = listOfArgs.map(e => Number(e));
+if (listOfArgs.length === 0) {
+  console.log(0);
+} else if (listOfArgs[0] === 1 || listOfArgs.length === 1) {
+  console.log(0);
+} else {
+  const max = Math.max(...listOfArgs);
+  const index = listOfArgs.indexOf(max);
+  const secondMax = Math.max(...listOfArgs.slice(0, index), ...listOfArgs.slice(index + 1));
+  console.log(secondMax);
+}

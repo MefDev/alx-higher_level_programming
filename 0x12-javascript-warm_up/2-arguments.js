280fbdbc7153b1555fb9e1cc861e468f8e28323a
mefdev <mohamedmeftah2018@gmail.com>
2023-11-13 22:03:21 +0100

    Arguments like piece of art

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-(argv.length === 2)
+(process.argv.length <= 2)
   ? console.log('No argument')
-  : (argv.length === 3)
+  : (process.argv.length <= 3)
       ? console.log('Argument found')
       : console.log('Arguments found');

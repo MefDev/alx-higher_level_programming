7638decfdf9f4bd42accc0dfcf346068caab4c0b
mefdev <mohamedmeftah2018@gmail.com>
2023-11-13 22:25:33 +0100

    Loop through argv or No argument printed

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const isArrayEmpty = (arr) => {
+  return Array.isArray(arr) && arr.every(() => false);
+}
+
+const argvList = process.argv.slice(2)
+!isArrayEmpty(argvList)
+  ? argvList.forEach((val) => console.log(val))
+  : console.log("No argument")
+
+
+
+
+

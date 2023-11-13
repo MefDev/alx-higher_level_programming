b1fc13dfacd49120df0d2d5118fb830db88bd8c3
mefdev <mohamedmeftah2018@gmail.com>
2023-11-13 23:16:01 +0100

    Multi language loop easy buzzy

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
+
+const langs = [c, python, javascript];
+langs.forEach(lang => console.log(lang));

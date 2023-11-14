ba007f7e4d14a97c157f799d944d4fda3d382cdf
mefdev <mohamedmeftah2018@gmail.com>
2023-11-14 01:00:09 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,7 +8,11 @@ if (listOfArgs.length === 0) {
   console.log(0);
 } else {
   const max = Math.max(...listOfArgs);
-  const index = listOfArgs.indexOf(max);
-  const secondMax = Math.max(...listOfArgs.slice(0, index), ...listOfArgs.slice(index + 1));
-  console.log(secondMax);
+  if (!isFinite(max)) {
+    console.log(0);
+  } else {
+    const index = listOfArgs.indexOf(max);
+    const secondMax = Math.max(...listOfArgs.slice(0, index), ...listOfArgs.slice(index + 1));
+    console.log(secondMax);
+  }
 }

865b279a73076e7c4ce0dc50823127d1f9a09ba1
mefdev <mohamedmeftah2018@gmail.com>
2023-11-15 01:19:17 +0100

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,10 +2,15 @@
 
 const fs = require('fs');
 
-const [fileA, fileB, fileC] = process.argv.slice(2);
+const [, , fileA, fileB, fileC] = process.argv;
 
-const contentA = fs.readFileSync(fileA, 'utf8');
-const contentB = fs.readFileSync(fileB, 'utf8');
+try {
+  const contentA = fs.readFileSync(fileA, 'utf8');
+  const contentB = fs.readFileSync(fileB, 'utf8');
 
-const content = `${contentA}\n${contentB}`;
-fs.writeFileSync(fileC, content, 'utf8');
+  const content = `${contentA}\n${contentB}`;
+
+  fs.writeFileSync(fileC, content, 'utf8');
+} catch (err) {
+  console.error('An error occurred:', err.message);
+}

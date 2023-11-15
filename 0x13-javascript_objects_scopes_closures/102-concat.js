2c202c962390d8816afab5e162ead3d8af44d1ed
mefdev <mohamedmeftah2018@gmail.com>
2023-11-15 01:13:17 +0100

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,38 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
 
 const [fileA, fileB, fileC] = process.argv.slice(2);
 
-const currentPath = process.cwd();
+const contentA = fs.readFileSync(fileA, 'utf8');
+const contentB = fs.readFileSync(fileB, 'utf8');
 
-const filePathA = path.join(currentPath, fileA);
-const filePathB = path.join(currentPath, fileB);
-const filePathC = path.join(currentPath, fileC);
-
-const readerA = readline.createInterface({
-  input: fs.createReadStream(filePathA),
-  output: process.stdout,
-  terminal: false
-});
-
-readerA.on('line', line => {
-  fs.appendFileSync(filePathC, `${line}\n`, 'utf8');
-});
-
-readerA.on('close', () => {
-  const readerB = readline.createInterface({
-    input: fs.createReadStream(filePathB),
-    output: process.stdout,
-    terminal: false
-  });
-  readerB.on('line', line => {
-    fs.appendFileSync(filePathC, `${line}`, 'utf8');
-  });
-
-  readerB.on('close', () => {
-
-  });
-});
+const content = `${contentA}\n${contentB}`;
+fs.writeFileSync(fileC, content, 'utf8');

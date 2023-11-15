fa0d47ad87d9c6a96a599c70a2b4faad4bbddd5a
mefdev <mohamedmeftah2018@gmail.com>
2023-11-15 01:05:05 +0100

    Sort a list

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-const list = [89, 90, 91, 92, 93, 94];
 const obj = {
-  1: [],
-  2: [],
-  3: []
 };
-for (let i = 0; i < list.length; i++) {
-  if (dict[list[i]] === 1) {
-    obj['1'].push(list[i]);
-  } else if (dict[list[i]] === 2) {
-    obj['2'].push(list[i]);
-  } else {
-    obj['3'].push(list[i]);
-  }
+for (count in dict){
+    const occurrences = dict[count];
+    if (!obj[occurrences]){
+        obj[occurrences] = []
+    }
+    obj[occurrences].push(count)
 }
+console.log(obj)
+   

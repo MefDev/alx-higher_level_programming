4feb717a907ae2ea25ab93bdda7da84f24a6b087
mefdev <mohamedmeftah2018@gmail.com>
2023-11-15 16:41:13 +0100

    Concat two files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,30 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const path = require('node:path');
 
 const [, , fileA, fileB, fileC] = process.argv;
 
-const contentA = fs.readFileSync(fileA, 'utf8');
-const contentB = fs.readFileSync(fileB, 'utf8');
+const currentPath = process.cwd();
 
-const content = `${contentA}\n${contentB}`;
+const filePathA = path.join(currentPath, fileA);
+const filePathB = path.join(currentPath, fileB);
+const filePathC = path.join(currentPath, fileC);
 
-fs.writeFileSync(fileC, content, 'utf8');
+fs.readFile(filePathA, 'utf8', (err, data) => {
+  if (err) console.log(err);
+  fs.writeFile(filePathC, `${data}\n`, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
+
+fs.readFile(filePathB, 'utf8', (err, data) => {
+  if (err) console.log(err);
+  fs.appendFile(filePathC, data, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});

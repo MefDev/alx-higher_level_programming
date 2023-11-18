491c5333db4de8662137a4f1fe1085f067772f9e
mefdev <mohamedmeftah2018@gmail.com>
2023-11-18 18:09:20 +0100

    Select state from states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=MY_USER, passwd=MY_PASS, db=MY_DB)
     cur = conn.cursor()
-    cur.execute("SELECT * from states")
+    cur.execute("SELECT * FROM `states`")
     states = cur.fetchall()
     # Get the states
     [print(state) for state in states]

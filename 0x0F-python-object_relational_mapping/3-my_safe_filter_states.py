340834d6f388560ea16a73146ae313749ead1b2d
mefdev <mohamedmeftah2018@gmail.com>
2023-11-18 19:52:22 +0100

    Avoid SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""get the filtered states from the database using the CLI arguments
+"""
+# Lists the filtered states from the database hbtn_0e_0_usa.
+# Usage: ./2-filter_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+
+
+def main(args):
+    """Select filtered states from MY_DB"""
+
+    # Arguments from the CLI
+    MY_USER, MY_PASS, MY_DB, STATE_NAME = args[1], args[2], args[3], args[4]
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = conn.cursor()
+    query = ("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+             ).format(STATE_NAME)
+    cur.execute(query)
+    states = cur.fetchall()
+    # Get the states
+    [print(state) for state in states]
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    main(argv)

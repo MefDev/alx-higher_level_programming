dc9b027433ea40598cc501dbe4548132cf4d223e
mefdev <mohamedmeftah2018@gmail.com>
2023-11-19 01:31:10 +0100

    Create a table class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,8 +4,10 @@
 from model_state import Base, State
 from sqlalchemy import Column, String, Integer, ForeignKey
 
+
 class City(Base):
     __tablename__ = 'cities'
-    id = Column(Integer,autoincrement=True, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey(State.id))

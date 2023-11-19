6bd6ff6d68fc0d2e14a8dc5348073bb45d234229
mefdev <mohamedmeftah2018@gmail.com>
2023-11-19 01:30:13 +0100

    Create a table class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""City class to table in database"""
+
+from model_state import Base, State
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer,autoincrement=True, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id))

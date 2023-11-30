6c3d9114df6e735ca92ade6730c165719b3ab89d
mefdev <mohamedmeftah2018@gmail.com>
2023-11-30 23:12:52 +0100

    get the body content

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # get the body
-status_code=$(curl -sI $1 | grep HTTP/1.1 | awk '{print $2}' 2>/dev/null)
-
-if [ "$status_code" = "200" ];
-then
-    curl -X GET $1
-fi
-
+status_code=$(curl -sI "$1" | grep "HTTP/1.1" | awk '{print $2}' 2>/dev/null); if [ "$status_code" = "200" ]; then curl -X GET "$1"; fi

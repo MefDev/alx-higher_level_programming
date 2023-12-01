0133af4ddb64f783b5f1f4bfb294ff6824fdd9a5
mefdev <mohamedmeftah2018@gmail.com>
2023-12-02 00:49:15 +0100

    Display the value of X-Request-Id

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,22 +3,13 @@
 import requests
 from sys import argv
 if __name__ == "__main__":
-    q = ""
-    if (len(argv) == 1):
-        q = ""
-        print("No result")
-    else:
-        res = requests.get('http://0.0.0.0:5000/search_user', )
-        if (len(res.json()) == 0):
-            print('No result')
-        try:
-            if (len(argv) > 1):
-                q = argv[1]
-            for obj in res.json():
-                if (obj['name'].startswith(q)):
-                    print("[{}] {}".format(obj['id'], obj['name']))
-                    exit()
-            print('No result')
-
-        except ValueError:
-            print("Not a valid JSON")
+    data = {'q': "" if len(argv) == 1 else argv[1]}
+    res = requests.get('http://54.237.23.212:5000/search_user', data=data)
+    try:
+        if res.json():
+            resp = res.json()
+            print("[{}] {}".format(resp.get('id'), resp.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")

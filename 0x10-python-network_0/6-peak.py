df8cf6b9ee3e5541836c869857de773efa80652d
mefdev <mohamedmeftah2018@gmail.com>
2023-12-01 01:15:47 +0100

    Find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,23 @@
 
 #!/usr/bin/python3
 """Find a peak number"""
+
+
 def find_peak_recursive(arr, left, right):
     """Find peak recusive"""
     mid = left + (right - left) // 2
-    
+
     if (mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == len(arr) - 1 or arr[mid + 1] <= arr[mid]):
-        return arr[mid] # find peak
-    
+        return arr[mid]  # find peak
+
     # If mid element is smaller than its left neighbor, search left
     elif mid > 0 and arr[mid - 1] > arr[mid]:
         return find_peak_recursive(arr, left, mid - 1)
-    
+
     # If mid element is smaller than its right neighbor, search right
     else:
         return find_peak_recursive(arr, mid + 1, right)
+
 
 def find_peak(list_of_integers):
     """Find a peak"""

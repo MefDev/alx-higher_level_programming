908266057ab2d136c73a9a89c3f65aae7e7e6caa
mefdev <mohamedmeftah2018@gmail.com>
2023-12-02 00:34:58 +0100

    get the id of the user

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Get the id of a usr from github"""
-import requests
-from sys import argv
-auth = (argv[1], argv[2])
-res = requests.get('https://api.github.com/user', auth=auth)
-print(res.json().get('id'))
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    auth = (argv[1], argv[2])
+    res = requests.get('https://api.github.com/user', auth=auth)
+    print(res.json().get('id'))

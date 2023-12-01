b976af884797d6ab833806e39234b2986b691a98
mefdev <mohamedmeftah2018@gmail.com>
2023-12-02 00:56:55 +0100

    Post an email to a server

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,8 +2,8 @@
 """displays the content of the res & post a req"""
 import requests
 from sys import argv
-
-url = argv[1]
-data = {'email': argv[2]}
-res = requests.post(url, data)
-print("{}".format(res.text))
+if __name__ == "__main__":
+    url = argv[1]
+    data = {'email': argv[2]}
+    res = requests.post(url, data)
+    print("{}".format(res.text))

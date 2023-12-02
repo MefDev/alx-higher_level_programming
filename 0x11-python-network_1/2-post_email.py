a5a7afe0e0eb1e0176769b72b547302fdb41c941
mefdev <mohamedmeftah2018@gmail.com>
2023-12-02 01:01:10 +0100

    post an email & display it

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,10 +2,10 @@
 """displays the value of the X-Request-Id found in the header of the res"""
 from urllib import request, parse
 from sys import argv
-
-url = argv[1]
-data = {'email': argv[2]}
-data = parse.urlencode(data).encode('ascii')
-req = request.Request(url, data)
-with request.urlopen(req) as res:
-    print("{}".format(res.read().decode('utf-8')))
+if __name__ == "__main__":
+    url = argv[1]
+    data = {'email': argv[2]}
+    data = parse.urlencode(data).encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as res:
+        print("{}".format(res.read().decode('utf-8')))

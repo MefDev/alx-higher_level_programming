abe4515b4d9cb3cc7f06c161c6525920d6139b3d
mefdev <mohamedmeftah2018@gmail.com>
2023-12-02 01:02:12 +0100

    Display the value of X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in the header of the response"""
+"""displays the value of the X-Request-Id found in the header of the res"""
 from urllib import request
 from sys import argv
-
-url = argv[1]
-with request.urlopen(url) as res:
-    print("{}".format(res.headers.get('X-Request-Id')))
+if __name__ == "__main__":
+    url = argv[1]
+    with request.urlopen(url) as res:
+        print("{}".format(res.headers.get('X-Request-Id')))

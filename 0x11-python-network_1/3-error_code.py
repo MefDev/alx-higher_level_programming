664825486a85d3d2a007888d22800435b3cd64f0
mefdev <mohamedmeftah2018@gmail.com>
2023-12-02 01:00:08 +0100

    handle Error code using urllib

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,11 +3,11 @@
 from urllib import request
 from urllib.error import HTTPError
 from sys import argv
+if __name__ == "__main__":
+    try:
+        url = argv[1]
+        with request.urlopen(url) as res:
+            print("{}".format(res.read().decode("utf-8")))
 
-try:
-    url = argv[1]
-    with request.urlopen(url) as res:
-        print("{}".format(res.read().decode("utf-8")))
-
-except HTTPError as e:
-    print("Error code: {}".format(e.status))
+    except HTTPError as e:
+        print("Error code: {}".format(e.status))

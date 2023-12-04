4128031cc4817259f8206c7b628aeaa29a3e7498
mefdev <mohamedmeftah2018@gmail.com>
2023-12-04 02:10:53 +0100

    List 10 commits used by rails

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,12 +7,14 @@ if __name__ == "__main__":
 
     owner = argv[1]
     repo = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits?per_page=11'.format(
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
         owner, repo)
 
     list_of_commiters = requests.get(url)
-
+    i = 10
     for committer in list_of_commiters.json():
-        sha = committer.get('sha')
-        name = committer.get('commit').get('author').get('name')
-        print("{}: {}".format(sha, name))
+        if (i > 0):
+            sha = committer.get('sha')
+            name = committer.get('commit').get('author').get('name')
+            print("{}: {}".format(sha, name))
+            i -= 1

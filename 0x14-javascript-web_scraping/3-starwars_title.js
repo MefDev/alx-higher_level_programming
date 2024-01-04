f518df266787dbab524ebcbf4f7b96b74625668b
mefdev <mohamedmeftah2018@gmail.com>
2024-01-04 01:55:10 +0100

    Grab the title of a movie from SWAPI

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const { argv } = require('process');
 const ID = argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${ID}`;
 
-request(url, function (error, response, body) {
-  const { title } = JSON.parse(body);
-  console.log(title);
+request(url, (error, response, body) => {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
 });
